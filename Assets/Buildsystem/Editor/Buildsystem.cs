e18da0ed5b49442c3d7b9563255bd3dba0e58a37
Marco H <mahe0046@stud.hs-kl.de>
2020-06-05 03:05:00 +0200

    add first Buildsystem with platform switch and test scenes

---
+++ Assets/Buildsystem/Editor/Buildsystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class Buildsystem : MonoBehaviour
+{
+    [MenuItem("Buildsystem/Platform/Standalone - Win64")]
+    private static void LoadStandaloneWin64Build()
+    {
+        AssetDatabase.DeleteAsset("Assets/GoogleVR");
+        AssetDatabase.DeleteAsset("Assets/HTC.UnityPlugin");
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+        EditorSceneManager.OpenScene("Assets/Buildsystem/Scenes/WinStandalone.unity");
+    }
+
+    [MenuItem("Buildsystem/Platform/LoadVIU - VivePro-Win64")]
+    private static void LoadViuViveProWin64Build()
+    {
+        AssetDatabase.DeleteAsset("Assets/GoogleVR");
+        AssetDatabase.DeleteAsset("Assets/HTC.UnityPlugin");
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+        //Open the Scene in the Editor (do not enter Play Mode)
+        EditorSceneManager.OpenScene("Assets/Buildsystem/Scenes/VIU-Win.unity");
+        AssetDatabase.ImportPackage("Assets/Resources/ViveInputUtility_v1.10.7.unitypackage", false);
+    }
+
+    [MenuItem("Buildsystem/Platform/LoadVIU - ViveFocusPro - Android")]
+    private static void LoadViveFocusProAndroidBuild()
+    {
+        AssetDatabase.DeleteAsset("Assets/GoogleVR");
+        AssetDatabase.DeleteAsset("Assets/HTC.UnityPlugin");
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        //Open the Scene in the Editor (do not enter Play Mode)
+        EditorSceneManager.OpenScene("Assets/Buildsystem/Scenes/VIU-Android.unity");
+        AssetDatabase.ImportPackage("Assets/Resources/ViveInputUtility_v1.10.7.unitypackage", false);
+    }
+
+    [MenuItem("Buildsystem/Platform/LoadGVR - Cardboard - Android")]
+    private static void LoadGvrCardboardAndroidBuild()
+    {
+        AssetDatabase.DeleteAsset("Assets/GoogleVR");
+        AssetDatabase.DeleteAsset("Assets/HTC.UnityPlugin");
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        EditorSceneManager.OpenScene("Assets/Buildsystem/Scenes/GVR.unity");
+        AssetDatabase.ImportPackage("Assets/Resources/GoogleVRForUnity_1.200.1.unitypackage", false);
+    }
+}
---
+++ Assets/Buildsystem/Editor/SceneConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public enum OptionsTargetGroup
+{
+    Standalone,
+    Android,
+}
+
+public enum OptionsBuildTarget
+{
+    StandaloneWindows64,
+    Android,
+}
+
+public class SceneConfiguration : EditorWindow
+{
+    public OptionsBuildTarget bt;
+    public OptionsTargetGroup btg;
+    bool assignVIU = false;
+    bool assignGvR = false;
+    bool assignWaveSDK = false;
+    
+
+    private List<Scene> allScenes = new List<Scene>();
+
+    [MenuItem("Buildsystem/Platform/Configuration")]
+    static void Init()
+    {
+        SceneConfiguration window =
+            (SceneConfiguration)EditorWindow.GetWindow(typeof(SceneConfiguration), true,
+                "Scene Configuration");
+        window.Show();
+    }
+
+    void OnEnable()
+    {
+        initSceneConfiguration();
+    }
+    void OnGUI()
+    {
+        drawSceneConfiguration();
+    }
+
+
+    void drawSceneConfiguration()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 250, 250));
+        GUILayout.Label("Scene Configuration");
+
+        bt = (OptionsBuildTarget)EditorGUILayout.EnumPopup("BuildTarget :",bt);
+        btg = (OptionsTargetGroup)EditorGUILayout.EnumPopup("BuildTargetGroup :", btg);
+        assignVIU = GUILayout.Toggle(assignVIU, "VIU");
+        assignGvR = GUILayout.Toggle(assignGvR, "Google VR");
+        assignWaveSDK = GUILayout.Toggle(assignWaveSDK, "Wave SDK");
+        GUILayout.EndArea();
+    }
+
+    void initSceneConfiguration()
+    {
+        for (int i = 0; i < EditorSceneManager.sceneCount; i++)
+        {
+            allScenes.Add(EditorSceneManager.GetSceneAt(i));
+            Debug.Log(EditorSceneManager.GetSceneAt(i).name);
+        }
+    }
+
+}
